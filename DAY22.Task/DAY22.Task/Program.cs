c2cf3ebf8179e0168fd508cca0630ec4c34816f7
Minvrl <130864645+Minvrl@users.noreply.github.com>
2024-01-23 02:21:17 +0400

    Add files via upload

---
+++ DAY22.Task/DAY22.Task/Program.cs
@@ -0,0 +1,33 @@
+﻿using DAY22.Task;
+
+
+string fullname;
+string groupno;
+string ageStr;
+byte age;
+
+do
+{
+    Console.Write("Telebenin ad,soyadini daxil edin - ");
+    fullname = Console.ReadLine();
+
+} while (!Student.CheckFullname(fullname));
+
+do
+{
+    Console.Write(" Qrup nomresini daxil edin - ");
+    groupno = Console.ReadLine();   
+
+} while (!Student.CheckGroupNo(groupno));
+
+do
+{
+    Console.Write("  Yasini daxil edin - ");
+    ageStr = Console.ReadLine();    
+
+} while (!byte.TryParse(ageStr, out age) || age < 0 );
+
+Student student = new Student() { Fullname = Student.CapitalizeFullname(fullname),GroupNo = groupno, Age = age };
+
+Console.WriteLine($"Ad,soyad - {student.Fullname} , Qrup nomresi - {student.GroupNo} , Yaş - {student.Age}");
+
---
+++ DAY22.Task/DAY22.Task/Student.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+
+namespace DAY22.Task
+{
+    internal class Student
+    {
+        private string _fullname;
+        private string _groupNO;
+        public byte Age;
+        public string Fullname
+        {
+            get => _fullname;
+            set
+            {
+                if (CheckFullname(value))
+                {
+                    _fullname = value;
+                }
+            }
+        }
+
+        public string GroupNo
+        {
+            get => _groupNO;
+            set
+            {
+                if(CheckGroupNo(value))
+                {
+                    _groupNO = value;
+                }
+            }
+        }
+
+
+        public static bool CheckFullname(string fullname)
+        {
+            if (string.IsNullOrWhiteSpace(fullname)) return false;
+
+            string[] parts = fullname.Split(' ');
+
+            if (parts.Length != 2) return false;
+
+           for (int i = 0;i < parts.Length; i++)
+            {
+                if (!IsLettersOnly(parts[i])) return false;
+            }
+            return true;
+
+           
+        }
+
+
+        public static bool CheckGroupNo(string groupno)
+        {
+            if (string.IsNullOrWhiteSpace(groupno) || groupno.Length < 4)
+            {
+                return false;
+            }
+
+            string letterPart = groupno.Substring(0, 1);
+            string numberPart = groupno.Substring(1);
+
+            if (IsLetter(letterPart) && IsDigit(numberPart))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+        #region Additional methods
+        protected static bool IsLettersOnly(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str)) return false;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!char.IsLetter(str[i])) return false;
+            }
+
+            return true;
+        }
+
+        private static bool IsLetter(string str)
+        {
+            return !string.IsNullOrEmpty(str) && str.Length == 1 && char.IsUpper(str[0]);
+        }
+
+        private static bool IsDigit(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!char.IsDigit(str[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static string CapitalizeFullname(string fullname)
+        {
+            string[] parts = fullname.Split(' ');
+
+            for (int i = 0; i < parts.Length; i++)
+            {
+                parts[i] = char.ToUpper(parts[i][0]) + parts[i].Substring(1).ToLower();
+            }
+
+            return string.Join(" ", parts);
+        }
+
+        #endregion
+
+    }
+}
